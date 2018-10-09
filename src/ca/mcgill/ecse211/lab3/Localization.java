38f8f081d1aed5976be900ea0dbb745824d7e338
Susan Matuszewski <susan.matuszewski@mail.mcgill.ca>
2018-10-09 09:44:14 -0400

    additonal comments

---
+++ src/ca/mcgill/ecse211/lab3/Localization.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse211.lab3;
+
+
+//use the us sensor to poll data and process it by detecting
+//either the rising or falling edge to figure out where it is
+//see Localization tutorial
+public class Localization extends Thread {
+
+}
